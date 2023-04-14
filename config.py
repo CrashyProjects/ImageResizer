bbb4ac8d1df8a338a5161c1e3753f7826c1b68bf
Crashy <121903140+CrashyProjects@users.noreply.github.com>
2023-04-14 18:31:18 +0200

    Implemented imageResizer

---
+++ config.py
@@ -0,0 +1,11 @@
+# Default directory path, will be used only if not passed as argument
+directory = "."  
+
+# Desired destination path, if set to "" will be the same has "directory"
+destination = ""
+
+# If True, a copy of the resized image will be saved as "RESIZED_filename.ext", if False, the original image will be overwritten
+saveCopy = True  
+
+# Desired resize dimensions in pixels (width, height)
+resizeRes = (1080, 1920)
---
+++ imageResizer.py
@@ -0,0 +1,54 @@
+import os
+import sys
+import imghdr
+
+from PIL import Image
+import config
+
+directory = config.directory
+saveCopy = config.saveCopy
+resizeRes = config.resizeRes
+destination = config.destination
+
+def is_image(filename):
+    image_types = {'jpg', 'jpeg', 'png', 'bmp', 'gif'}
+    file_type = imghdr.what(filename)
+    if file_type in image_types:
+        return True
+    else:
+        return False
+
+# Set the directory path
+if len(sys.argv) > 1:
+    directory = sys.argv[1]
+print("----------------------------------")
+print("Using directory: " + directory)
+print("----------------------------------")
+if not os.path.isdir(directory):
+    print("!Directory not found")
+    sys.exit(1)
+
+#Set destination path
+if destination == "":
+    print("Destination set to: " + directory)
+    destination = directory
+else:
+    if not os.path.isdir(destination):
+        print("!Destination path not found, creating it")
+        os.mkdir(destination)
+
+# Get a list of all images in the directory
+files = [file for file in os.listdir(directory) if os.path.isfile(os.path.join(directory, file)) and is_image(os.path.join(directory, file))]
+    
+# Conversion
+for file in files:
+    image = Image.open(os.path.join(directory, file))
+    if(saveCopy): #Do not overwrite
+        file = "RESIZED_" + file
+    name = os.path.join(destination, file)
+    if image.size == resizeRes:
+        print("!File is alredy ", resizeRes, " : ", name)
+    else:
+        resized_image = image.resize(resizeRes)
+        resized_image.save(name)
+        print("Resized: " + name)
